e798e2cf7f96c90ddd611d86baeed15830acc444
root <root@DESKTOP-04PNBN5>
2023-08-01 15:45:17 +0200

    InputBase component

--- front/src/pages/HomePage/HomePage.tsx
+++ front/src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,19 @@
-import { FC } from "react";
-import CarbonButton from "../../components/CarbonButton";
-import TextField from "../../components/CarbonInput";
-import theme from "../../theme";
-
+import { FC } from 'react';
+import CarbonButton from '../../components/CarbonButton';
+import TextField from '../../components/CarbonInputBase';
+import theme from '../../theme';
 
 const HomePage: FC = () => {
-    return(
-        <>
-            <CarbonButton color="primary">Salut</CarbonButton>
-            <CarbonButton color="secondary">Au revoir</CarbonButton>
-            <TextField/>
-            <TextField/>
-            <TextField />
-            {/* <CarbonIcon><AndroidIcon/></CarbonIcon> */}
-        </>
-    )
+  return (
+    <>
+      <CarbonButton color="primary">Salut</CarbonButton>
+      <CarbonButton color="secondary">Au revoir</CarbonButton>
+      <TextField />
+      <TextField />
+      <TextField />
+      {/* <CarbonIcon><AndroidIcon/></CarbonIcon> */}
+    </>
+  );
 };
 
 export default HomePage;
--- front/src/theme.ts
+++ front/src/theme.ts
@@ -1,36 +1,38 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
+
+// Needed to add variables to theme
+declare module '@mui/material/styles' {
+  interface Theme {
+    styleInputBase: {};
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    styleInputBase?: {};
+  }
+}
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#1CAF68"
-      },
-      secondary: {
-        main: "#3C8962"
-      },
-      background: {
-        default: "#3C8962"
-      },
+  palette: {
+    primary: {
+      main: '#1CAF68',
     },
-    typography: {
-        allVariants: {
-            fontFamily: "Roboto mono"
-        }
+    secondary: {
+      main: '#3C8962',
     },
-    components: {
-        MuiTextField: {
-          styleOverrides: {
-            root: {
-              '--TextField-brandBorderColor': '#1CAF68',
-              '--TextField-brandBorderHoverColor': '#3C8962',
-              '--TextField-brandBorderFocusedColor': '#3C8962',
-              '& label.Mui-focused': {
-                color: 'var(--TextField-brandBorderFocusedColor)',
-              },
-            },
-          },
-        },
-    }
+    background: {
+      default: '#3C8962',
+    },
+  },
+  typography: {
+    allVariants: {
+      fontFamily: 'Roboto mono',
+    },
+  },
+  styleInputBase: {
+    border: '1px solid',
+    height: '6vh',
+    borderColor: '#1CAF68',
+  },
 });
 
 export default theme;
