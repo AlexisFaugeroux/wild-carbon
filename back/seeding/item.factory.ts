7aed1a5f84238aa071c6e00c6740ff91f51657db
root <root@DESKTOP-04PNBN5>
2023-08-02 11:43:30 +0200

    Seeder class and seeding script

--- back/seeding/item.factory.ts
+++ back/seeding/item.factory.ts
@@ -1,22 +1,12 @@
 import { Faker } from '@faker-js/faker';
 import { setSeederFactory } from 'typeorm-extension';
 import { Item } from '../src/entity/Item';
-
-enum UnitEnum {
-  KG = 'kg',
-  KM = 'km',
-  L = 'l',
-}
+import UnitEnum from '../src/enum/unitEnum';
 
 export const ItemFactory = setSeederFactory(Item, (faker: Faker) => {
   const item = new Item();
   item.label = faker.lorem.word();
-  item.emissionFactor = faker.lorem.word({
-    length: {
-      min: 1,
-      max: 2,
-    },
-  });
+  item.emissionFactor = faker.number.float();
   item.unit = faker.helpers.enumValue(UnitEnum);
 
   return item;
