0e60fffd1a36e599a45e471a209973f9ef267395
root <root@DESKTOP-04PNBN5>
2023-08-02 15:18:26 +0200

    Fix Expense Resolver

--- back/src/index.ts
+++ back/src/index.ts
@@ -8,12 +8,23 @@ import CategoryResolver from './resolver/CategoryResolver';
 import ArticleResolver from './resolver/ArticleResolver';
 import ExpenseResolver from './resolver/ExpenseResolver';
 import ItemResolver from './resolver/ItemResolver';
+import { User } from './entity/User';
+
+export interface Context {
+  jwtPayload: User;
+}
 
 const start = async (): Promise<void> => {
   await dataSource.initialize();
 
   const typeGraphQLgeneratedSchema = await buildSchema({
-    resolvers: [UserResolver, CategoryResolver, ArticleResolver , ExpenseResolver , ItemResolver],
+    resolvers: [
+      UserResolver,
+      CategoryResolver,
+      ArticleResolver,
+      ExpenseResolver,
+      ItemResolver,
+    ],
     authChecker: ({ context }) => {
       console.log('context from authchecker', context);
       if (context.email !== undefined) {
@@ -26,18 +37,18 @@ const start = async (): Promise<void> => {
 
   const server = new ApolloServer({
     schema: typeGraphQLgeneratedSchema,
-    context: ({ req }) => {
+    context: async ({ req }) => {
       if (
-        req.headers.authorization !== undefined &&
-        req.headers.authorization !== ''
-      ) {
-        const payload = jwt.verify(
-          req.headers.authorization.split('Bearer ')[1],
-          'supersecretkey',
-        );
-        return payload;
-      }
-      return {};
+        req.headers.authorization === undefined ||
+        req.headers.authorization === ''
+      )
+        throw new Error('Authorization headers not set');
+
+      const payload = jwt.verify(
+        req.headers.authorization.split('Bearer ')[1],
+        'supersecretkey',
+      ) as User;
+      return { jwtPayload: payload };
     },
   });
 
--- back/src/resolver/ExpenseResolver.ts
+++ back/src/resolver/ExpenseResolver.ts
@@ -1,4 +1,5 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Context } from '../index';
 import dataSource from '../utils';
 import { Expense } from '../entity/Expense';
 import { User } from '../entity/User';
@@ -6,79 +7,77 @@ import { Item } from '../entity/Item';
 
 @Resolver()
 class ExpenseResolver {
-    @Mutation(() => String)
-    async createExpense(
-        @Arg('itemId') id : Item,
-        @Arg('userId')  idUser: User,
-        @Arg('title') title: string,
-        @Arg('quantity') quantity: number,
-        @Arg('emissionFactorTotal') emissionFactorTotal: number,
-    ) : Promise<string> {
-        const expense = new Expense();
+  @Mutation(() => String)
+  async createExpense(
+    @Arg('itemId') itemId: string,
+    @Arg('title') title: string,
+    @Arg('quantity') quantity: number,
+    @Arg('emissionFactorTotal') emissionTotal: number,
+    @Ctx() contextValue: Context,
+  ): Promise<string> {
+    const expense = new Expense();
 
-        const item = await dataSource
-            .getRepository(Item)
-            .findOneByOrFail( id );
-        if (!item) {
-            throw new Error('Item introuvable dans la base de données');
-        }
+    const item = await dataSource
+      .getRepository(Item)
+      .findOneByOrFail({ id: itemId });
+    if (!item) {
+      throw new Error('Item introuvable dans la base de données');
+    }
+    expense.item = item;
+    expense.user = contextValue.jwtPayload;
+    expense.title = title;
+    expense.emissionTotal = emissionTotal;
+    expense.quantity = quantity;
+    expense.createdAt = new Date();
 
-        expense.item = id;
-        expense.user = idUser;
-        expense.title = title;
-        expense.emissionFactorTotal = emissionFactorTotal;
-        expense.quantity = quantity;
-        expense.createdAt = new Date();
+    await dataSource.getRepository(Expense).save(expense);
 
-        await dataSource.getRepository(Expense).save(expense);
+    return 'Expense created';
+  }
 
-        return 'Expense created';
+  @Mutation(() => Number)
+  async calculatingEmissionFactor(
+    @Arg('itemId') itemId: string,
+    @Arg('quantity') quantity: number,
+  ): Promise<number> {
+    const item = await dataSource
+      .getRepository(Item)
+      .findOneByOrFail({ id: itemId });
+
+    if (!item) {
+      throw new Error('Item introuvable dans la base de données');
     }
 
-    @Mutation(() => Number)
-    async calculatingEmissionFactor(
-        @Arg('itemId') id : Item,
-        @Arg('quantity') quantity: number
-    ) : Promise<number> {
+    const emissionFactorCO2 = item.emissionFactor * quantity;
+    return emissionFactorCO2;
+  }
 
-        const item = await dataSource
-            .getRepository(Item)
-            .findOneByOrFail( id );
+  @Mutation(() => String)
+  async deleteExpense(@Arg('expenseId') id: string): Promise<String> {
+    const targetedExpense = await dataSource
+      .getRepository(Expense)
+      .findOneByOrFail({ id });
 
-        if (!item) {
-            throw new Error('Item introuvable dans la base de données');
-        }
+    await dataSource.getRepository(Expense).remove(targetedExpense);
+    return 'Expense deleted';
+  }
 
-        const emissionFactorCO2 = item.emissionFactor * quantity;
-        return emissionFactorCO2;
+  @Query(() => Expense)
+  async getExpense(@Arg('expenseId') id: string): Promise<Expense> {
+    const expense = await dataSource
+      .getRepository(Expense)
+      .findOneByOrFail({ id });
+    return expense;
+  }
+
+  @Query(() => [Expense])
+  async getAllExpenses(): Promise<Expense[]> {
+    try {
+      const expenses = await dataSource.getRepository(Expense).find();
+      return expenses;
+    } catch (error) {
+      throw error;
     }
-
-    @Mutation(() => String)
-    async deleteExpense(@Arg('expenseId') id: string): Promise<String>{
-        const targetedExpense = await dataSource
-            .getRepository(Expense)
-            .findOneByOrFail({id});
-
-            await dataSource.getRepository(Expense).remove(targetedExpense);
-            return 'Expense deleted';
-    }
-
-    @Query(() => Expense)
-    async getExpense(@Arg('expenseId') id: string): Promise<Expense>{
-        const expense = await dataSource
-            .getRepository(Expense)
-            .findOneByOrFail({id})
-        return expense
-    }
-
-    @Query(() => [Expense])
-    async getAllExpenses(): Promise<Expense[]>{
-        try{
-            const expenses = await dataSource.getRepository(Expense).find();
-            return expenses;
-        } catch (error) {
-            throw error;
-        }
-    }
+  }
 }
 export default ExpenseResolver;
