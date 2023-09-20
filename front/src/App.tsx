9467ce1e5d5db21422c2805a005e92c8564617d9
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2023-09-20 15:30:01 +0200

    fix: added context provider to App

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,15 +6,18 @@ import { Box, ThemeProvider } from '@mui/material';
 import theme from './theme';
 import Footer from './components/Footer';
 import Navigator from './Navigator';
+import { LoginContextProvider } from './hooks/useLoginContext';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-        <HeaderBar />
-        <Navigator />
-        <Footer />
-      </Box>
+      <LoginContextProvider>
+        <Box>
+          <HeaderBar />
+          <Navigator />
+          <Footer />
+        </Box>
+      </LoginContextProvider>
     </ThemeProvider>
   );
 }
