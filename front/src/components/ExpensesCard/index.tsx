925921eff4b02fbcbb2031ac6828b02a45cbec9f
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2024-01-30 15:25:22 +0100

    fix: replace dashboard grid with masonry

--- front/src/components/ExpensesCard/index.tsx
+++ front/src/components/ExpensesCard/index.tsx
@@ -1,74 +1,38 @@
-import { Box, Theme, Typography, useMediaQuery } from "@mui/material";
-
-import CarbonCard from "../CarbonCard";
-import ExpensesListItem from "../ExpensesListItem";
-import { DirectionsCar, Restaurant, Train } from "@mui/icons-material";
+import CarbonCard from '../CarbonCard';
+import ExpensesListItem from '../ExpensesListItem';
+import { DirectionsCar, Restaurant, Train } from '@mui/icons-material';
 
 export default function ExpensesCard() {
-  const isLg = useMediaQuery((theme: Theme) => theme.breakpoints.up("lg"));
-  const isPortrait = useMediaQuery("(orientation: portrait)");
-  const isLandscape = useMediaQuery("(orientation: landscape)");
-
-  const calculateHeight = () => {
-    if (isLg) {
-      return "30vh";
-    } else if (isPortrait) {
-      return "25vh";
-    } else if (isLandscape) {
-      return "45vh";
-    }
-  };
-
   return (
-    <Box sx={{ width: "100%" }}>
-      <CarbonCard
-        style={{
-          backgroundColor: "white",
-          border: "2px solid #3C8962",
-          margin: 0,
-          height: calculateHeight(),
-        }}
+    <CarbonCard
+      title="Mes dépenses carbones récentes"
+      style={{
+        backgroundColor: 'white',
+        border: '2px solid #3C8962',
+      }}
+    >
+      <CarbonCard.Content
+        sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
       >
-        <Typography
-          variant="body1"
-          sx={{
-            fontWeight: "bold",
-            fontStyle: "italic",
-            fontFamily: "Roboto",
-            fontSize: {
-              xs: "0.85rem",
-              sm: "0.95rem",
-              md: "1.1rem",
-              lg: "1.3rem",
-              xl: "1.5rem",
-            },
-          }}
-        >
-          Mes dépenses carbones récentes:
-        </Typography>
-        <CarbonCard.Content
-          sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
-        >
-          <ExpensesListItem
-            IconExpense={DirectionsCar}
-            name="Trajet voiture"
-            date="20 septembre"
-            expenseNumber="30"
-          />
-          <ExpensesListItem
-            IconExpense={Restaurant}
-            name="Mcdo"
-            date="10 septembre"
-            expenseNumber="2"
-          />
-          <ExpensesListItem
-            IconExpense={Train}
-            name="Train Lille-Paris"
-            date="05 septembre"
-            expenseNumber="10"
-          />
-        </CarbonCard.Content>
-      </CarbonCard>
-    </Box>
+        <ExpensesListItem
+          IconExpense={DirectionsCar}
+          name="Trajet voiture"
+          date="20 septembre"
+          expenseNumber="30"
+        />
+        <ExpensesListItem
+          IconExpense={Restaurant}
+          name="Mcdo"
+          date="10 septembre"
+          expenseNumber="2"
+        />
+        <ExpensesListItem
+          IconExpense={Train}
+          name="Train Lille-Paris"
+          date="05 septembre"
+          expenseNumber="10"
+        />
+      </CarbonCard.Content>
+    </CarbonCard>
   );
 }
