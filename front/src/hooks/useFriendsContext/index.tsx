984cd7e942b249f7e6bba6f96baafa23226b27c3
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2024-01-30 10:46:47 +0100

    fix: changed friend list query variables

--- front/src/hooks/useFriendsContext/index.tsx
+++ front/src/hooks/useFriendsContext/index.tsx
@@ -22,10 +22,11 @@ export const FriendsContextProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const { userId } = useContext(LoginContext);
+  console.log('userId', userId);
   const { data, error, loading, refetch } = useQuery<UserQuery>(GET_USER, {
-    variables: { userId },
+    variables: { id: userId },
   });
-
+  console.log('data', data);
   const value = useMemo(
     () => ({
       data,
