e014583b6e0748f3c894de16d1e3d6c0c11d0e4a
Alexis Faugeroux <root@DESKTOP-04PNBN5>
2024-02-29 15:41:39 +0100

    feat: clean data in db

--- back/src/seeding/main.seeder.ts
+++ back/src/seeding/main.seeder.ts
@@ -1,62 +1,60 @@
-import { DataSource } from 'typeorm';
-import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { faker } from '@faker-js/faker';
+import * as argon2 from 'argon2';
+import { DataSource } from 'typeorm';
+import { Seeder } from 'typeorm-extension';
 import { Article } from '../entity/Article';
 import { Category } from '../entity/Category';
 import { Item } from '../entity/Item';
 import { User } from '../entity/User';
-import { Categories } from '../enum/categoriesEnum';
+import { articlesFixture } from './fixtures/articles';
+import { categoriesFixture } from './fixtures/categories';
 import { itemsFixture } from './fixtures/items';
+import { usersFixture } from './fixtures/users';
 
 export class MainSeeder implements Seeder {
-  public async run(
-    dataSource: DataSource,
-    factoryManager: SeederFactoryManager,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ): Promise<any> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async run(dataSource: DataSource): Promise<any> {
     const userRepository = dataSource.getRepository(User);
     const articleRepository = dataSource.getRepository(Article);
     const itemRepository = dataSource.getRepository(Item);
     const categoryRespository = dataSource.getRepository(Category);
 
-    const articleFactory = factoryManager.get(Article);
-    const userFactory = factoryManager.get(User);
-
     console.log('Seeding: Processing Users...');
-    const users = await userFactory.saveMany(10);
+    const hashedPassword = await Promise.all(
+      usersFixture.map((userFixture) => argon2.hash(userFixture.password)),
+    );
+    const users = usersFixture.map((userFixture, index) => {
+      const user = new User();
+      user.pseudo = userFixture.pseudo;
+      user.email = userFixture.email;
+      user.password = hashedPassword[index];
+
+      return user;
+    });
     users.forEach((user) => {
       const friends = faker.helpers.arrayElements(users, {
         min: 0,
-        max: 8,
+        max: 2,
       });
       user.users = friends;
     });
     await userRepository.save(users);
 
-    console.log('Processing Categories...');
-    const categoriesArray: Categories[] = [];
-    Object.values(Categories).forEach((cat) => categoriesArray.push(cat));
-    const categories = Array(4)
-      .fill('')
-      .map((_, index) => {
-        const category = new Category();
-        category.name = categoriesArray[index];
-        return category;
-      });
-    await categoryRespository.save(categories);
+    console.log('Seeding: Processing Categories...');
+    await categoryRespository.save(categoriesFixture);
     const categoriesFromDb = await categoryRespository.find();
 
     console.log('Seeding: Processing Articles...');
-    const articles = await Promise.all(
-      Array(6)
-        .fill('')
-        .map(async () => {
-          const article = await articleFactory.make({
-            user: faker.helpers.arrayElement(users),
-          });
-          return article;
-        }),
-    );
+    const articles = articlesFixture.map((articleFixture, index) => {
+      const article = new Article();
+      article.title = articleFixture.title;
+      article.description = articleFixture.description;
+      article.url = articleFixture.url;
+      article.createdAt = new Date();
+      article.user = users[index];
+
+      return article;
+    });
     await articleRepository.save(articles);
 
     console.log('Seeding: Processing Items...');
